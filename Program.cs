3a89301be4b7d25b8213cac432c32c7b3b5b4722
Shantanu3340 <shantusodnar3340@gmail.com>
2025-06-30 15:52:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProjectForGit
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a = 10, b = 10, c;
+            c = a + b;
+            Console.WriteLine("add="+c);
+        }
+            
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Add();
+            Console.ReadLine(); 
+        }
+    }
+}
